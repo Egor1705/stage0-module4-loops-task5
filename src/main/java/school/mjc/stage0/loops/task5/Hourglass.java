ea088e75618f168e6ce60b86d4f68be6358b5cee
Egor1705 <BsuHelp@tut.by>
2022-08-20 13:51:02 +0300

    figures

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -3,5 +3,11 @@ package school.mjc.stage0.loops.task5;
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
 
+//        for(int i=0; i<height/2;i++){
+//            for(int j=0;j<height;j++) {
+//
+//            }
+//
+//    }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -1,7 +1,20 @@
 package school.mjc.stage0.loops.task5;
 
 public class Square {
-    public void printSquareFrom8s(int sideLength){
+    public void printSquareFrom8s(int sideLength) {
+        for (int i = 0; i <= sideLength - 1; i++) {
+
+            for (int j = 0; j < sideLength; j++) {
+                if (j > 0 && j < sideLength - 1 && i > 0 && i < sideLength - 1) {
+                    System.out.print(" ");
+                    continue;
+                }
+                System.out.print("8");
+            }
+            System.out.println();
+
+        }
 
     }
-}
+    }
+
